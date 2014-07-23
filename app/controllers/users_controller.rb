c6466acb7fa469e31d4643a60f286e5af45e5edf
Josh Weinstein <weinstein.josh@gmail.com>
2014-07-23 18:54:20 +0700

    Don't require login on Users#new/#create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :require_login, only: [:update, :destroy]
+  skip_before_filter :require_login, only: [:new, :create]
 
   def new
     @user = User.new
