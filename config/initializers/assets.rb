498bd91d18583abab6427a1ee8fd2f39e98e6ddf
Josh Weinstein <weinstein.josh@gmail.com>
2014-08-05 12:04:55 +0700

    Serve assets from S3.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,24 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile = [
+  'application.css','application.js',
+  'bookmarklet.css',
+
+  'flatty/fontawesome-webfont.eot',
+  'flatty/fontawesome-webfont.svg',
+  'flatty/fontawesome-webfont.ttf',
+  'flatty/fontawesome-webfont.woff',
+
+  'flatty/montserrat-regular-webfont.eot',
+  'flatty/montserrat-regular-webfont.svg',
+  'flatty/montserrat-regular-webfont.ttf',
+  'flatty/montserrat-regular-webfont.woff',
+
+  'flatty/montserrat-bold-webfont.eot',
+  'flatty/montserrat-bold-webfont.svg',
+  'flatty/montserrat-bold-webfont.ttf',
+  'flatty/montserrat-bold-webfont.woff',
+
+  'ZeroClipboard.swf'
+]
